b28a89dcee80226433a7f1df3dc9ad69c10415c9
LeoLuz <leoluz@users.noreply.github.com>
2023-08-21 22:53:58 -0400

    Update kitty configs

--- install.sh
+++ install.sh
@@ -7,10 +7,9 @@ kitty_cfgdir="$HOME/.config/kitty"
 mkdir -p $kitty_cfgdir
 [ ! -L $kitty_cfgdir/kitty.conf ] && ln -s $script_dir/kitty.conf $kitty_cfgdir/kitty.conf
 [ ! -d $kitty_cfgdir/kitty-themes ] && git clone --depth 1 https://github.com/dexpota/kitty-themes.git $kitty_cfgdir/kitty-themes
-[ ! -d $kitty_cfgdir/tokyonight-theme ] && git clone --depth 1 https://github.com/folke/tokyonight.nvim.git $kitty_cfgdir/tokyonight-theme
 [ ! -d $kitty_cfgdir/kittens ] && mkdir $kitty_cfgdir/kittens && ln -s $script_dir/kitty/kittens/zoom_toggle.py $kitty_cfgdir/kittens/zoom_toggle.py
 [ ! -L $kitty_cfgdir/wallpaper.png ] && ln -s $script_dir/wallpaper/eva.png $kitty_cfgdir/wallpaper.png
-[ ! -L $kitty_cfgdir/theme.conf ] && ln -s $kitty_cfgdir/tokyonight-theme/extras/kitty/tokyonight_night.conf $kitty_cfgdir/theme.conf
+[ ! -L $kitty_cfgdir/theme.conf ] && ln -s $script_dir/kitty/catppuccin-mocha.conf $kitty_cfgdir/theme.conf
 [ ! -L $kitty_cfgdir/session.conf ] && ln -s $script_dir/kitty/session.conf $kitty_cfgdir/session.conf
 
 # Oh-my-zsh setup
@@ -24,5 +23,3 @@ mkdir -p $kitty_cfgdir
 [ ! -f $HOME/.zshrc ] && ln -s $script_dir/zshrc $HOME/.zshrc
 [ ! -f $HOME/.gitconfig ] && ln -s $script_dir/gitconfig $HOME/.gitconfig
 #[ ! -f $HOME/.bash_profile ] && ln -s $script_dir/bash_profile $HOME/.bash_profile
-#
-#
