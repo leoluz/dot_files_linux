d1640f7d975581f1298d0f0b6d46d96bf5418271
LeoLuz <leoluz@users.noreply.github.com>
2024-03-03 14:14:58 -0500

    install on fedora

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #! /usr/bin/env sh
 
-script_dir=$(dirname $(python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "$0"))
+current_script_dir() {
+    SOURCE=${BASH_SOURCE[0]}
+    while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+      DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+      SOURCE=$(readlink "$SOURCE")
+      [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    echo $( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+}
+script_dir=$(current_script_dir)
 
 # Kitty setup
 kitty_cfgdir="$HOME/.config/kitty"
@@ -12,13 +21,16 @@ mkdir -p $kitty_cfgdir
 [ ! -L $kitty_cfgdir/session.conf ] && ln -s $script_dir/kitty/session.conf $kitty_cfgdir/session.conf
 
 # Oh-my-zsh setup
-#ohmyzsh_cfgdir="$HOME/.oh-my-zsh"
-#[ ! -d $ohmyzsh_cfgdir ] && git clone git@github.com:ohmyzsh/ohmyzsh.git $ohmyzsh_cfgdir
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# FZF
+if [ ! -d "$HOME/.fzf" ]; then
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install
+fi
 
 # Misc setup
-#[ ! -f $HOME/.ad_profile ] && ln -s $script_dir/ad_profile $HOME/.ad_profile
-#[ ! -f $HOME/.dw_profile ] && ln -s $script_dir/dw_profile $HOME/.dw_profile
-#[ ! -f $HOME/.it_profile ] && ln -s $script_dir/it_profile $HOME/.it_profile
 [ ! -f $HOME/.zshrc ] && ln -s $script_dir/zshrc $HOME/.zshrc
 [ ! -f $HOME/.gitconfig ] && ln -s $script_dir/gitconfig $HOME/.gitconfig
-#[ ! -f $HOME/.bash_profile ] && ln -s $script_dir/bash_profile $HOME/.bash_profile
